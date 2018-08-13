63c3247185ec3b9d9b8bea547f75c0903efe0eda
yaqin11 <2321093453@qq.com>
2018-08-13 15:28:36 +0800

    dingdong commit

--- E-commerce website/static/home/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var slide = new Swiper("#slider",{
-        direction:'horizontal',
-        loop:true,
-        speed:500,
-        autoplay:2000,
-        pagination: '.swiper-pagination'
-    })
-});
--- introduce_movie/introduce_movie/__init__.py
+++
@@ -1,3 +0,0 @@
-import pymysql
-#数据库的驱动
-pymysql.install_as_MySQLdb()
